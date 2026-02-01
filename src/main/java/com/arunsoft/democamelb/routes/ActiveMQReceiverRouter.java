bf04eee27c00fc57f930e5bd1f019f02d7c95a24
arunkumars <arunkumar.ntk@gmail.com>
2026-02-01 22:53:17 +0530

    Update 3

--- src/main/java/com/arunsoft/democamelb/routes/ActiveMQReceiverRouter.java
+++ src/main/java/com/arunsoft/democamelb/routes/ActiveMQReceiverRouter.java
@@ -1,6 +1,7 @@
 package com.arunsoft.democamelb.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,8 @@ public class ActiveMQReceiverRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("activemq:my-activemq-queue")
+                .log("${body}")
+                .unmarshal().json(JsonLibrary.Jackson,CurrencyExchange.class)
                 .to("log:received-message-from-active-mq");
     }
 }
