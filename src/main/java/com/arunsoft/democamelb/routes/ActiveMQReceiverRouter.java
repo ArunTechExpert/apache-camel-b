1f7d257a48a64fffae66f1956ea03a0f447f51ae
arunkumars <arunkumar.ntk@gmail.com>
2026-02-02 13:08:33 +0530

    Rest Cosumer

--- src/main/java/com/arunsoft/democamelb/routes/ActiveMQReceiverRouter.java
+++ src/main/java/com/arunsoft/democamelb/routes/ActiveMQReceiverRouter.java
@@ -2,15 +2,35 @@ package com.arunsoft.democamelb.routes;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.math.BigDecimal;
+
+//@Component
 public class ActiveMQReceiverRouter extends RouteBuilder {
+    @Autowired
+    private MyCurrencyExchangeTransformer  myCurrencyExchangeTransformer;
+
     @Override
     public void configure() throws Exception {
         from("activemq:my-activemq-queue")
                 .log("${body}")
                 .unmarshal().json(JsonLibrary.Jackson,CurrencyExchange.class)
+                .bean(myCurrencyExchangeTransformer, "processMessage")
                 .to("log:received-message-from-active-mq");
+    }
+}
+
+//@Component
+class MyCurrencyExchangeTransformer{
+    Logger logger = LoggerFactory.getLogger(MyCurrencyExchangeTransformer.class);
+
+    public CurrencyExchange processMessage(CurrencyExchange exchange) {
+        exchange.setConversionMultiple(exchange.getConversionMultiple().add(exchange.getConversionMultiple().multiply(BigDecimal.TEN)));
+        logger.info(exchange.toString());
+        return exchange;
     }
 }
---
+++ src/main/java/com/arunsoft/democamelb/routes/CurrencyExchangeController.java
@@ -0,0 +1,17 @@
+package com.arunsoft.democamelb.routes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange findConversionvalue(@PathVariable String from, @PathVariable String to) {
+        return new CurrencyExchange(1004L, from, to, BigDecimal.TEN);
+    }
+
+}
